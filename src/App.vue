7c3cef27433073dd3042cc6878783b08e648d7a6
Amir Khusenov <husenova270@gmail.com>
2025-02-27 17:34:22 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+ <script setup>
+  import {defineAsyncComponent} from 'vue'
+
+  const NavBar = defineAsyncComponent(() =>import('./components/NavBar.vue'))
+  const HeroSection = defineAsyncComponent(() =>import('./components/HeroSection.vue'))
+  const ServicesSection = defineAsyncComponent(() =>import('./components/ServicesSection.vue'))
+  const AboutSection = defineAsyncComponent(() =>import('./components/AboutSection.vue'))
+  const ExperienceAndSkills = defineAsyncComponent(() =>import('./components/ExperienceAndSkills.vue'))
+  const ContactSection = defineAsyncComponent(() =>import('./components/ContactSection.vue'))
+  const TestimonialsSection = defineAsyncComponent(() =>import('./components/TestimonialsSection.vue'))
+</script>
+
+<template>
+    <div class="bg-[#111827] min-h-screen">
+      <NavBar />
+      <HeroSection />
+      <ServicesSection />
+      <AboutSection />
+      <ExperienceAndSkills />
+      <ContactSection />
+      <TestimonialsSection />
+    </div>
+</template>
+
+
